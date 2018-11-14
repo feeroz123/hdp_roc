1b005060f365decbf7a447fc1ab5205612046767
Feeroz Alam <feeroz123@gmail.com>
2018-11-14 13:59:09 +0530

    updated files

--- env_configurations.sh
+++
@@ -1,8 +0,0 @@
-# This configuration file contains all configurable varibles and properties that get used by the deployment scripts
-
-################# Server level variables ################
-export AMBARI_SERVER_HOSTNAME=master.subex.com
-export AMBARI_ADMIN_PASSWORD=admin
-export CODE_REPO=http://github.com/feeroz123/hdp_roc
-export CLUSTER_NAME=single-node-hdp-cluster
-export HDP_STACK_VERSION=2.6
