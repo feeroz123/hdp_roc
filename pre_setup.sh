ef6daebc1f022a8a138981c77b4a6c6f230da4e7
Feeroz Alam <feeroz123@gmail.com>
2018-11-24 17:58:54 +0530

    Updated steps

--- pre_setup.sh
+++ pre_setup.sh
@@ -1,3 +1,5 @@
+# As ROOT user:
+# --------------
 #### Update YUM and install tools
 yum update
 yum install -y wget scp curl openssl unzip tar ansible git
@@ -6,12 +8,37 @@ yum install -y wget scp curl openssl unzip tar ansible git
 cp .bashrc_hdp $HOME
 source $HOME/.bashrc_hdp
 
+### Set hostname 
+echo $AMBARI_SERVER_HOSTNAME > /etc/hostname
+
+### Set the hostname in Network
+vi /etc/sysconfig/network	=> set HOSTNAME=$AMBARI_SERVER_HOSTNAME
+
+### Update hostname entry in hosts file
+vi /etc/hosts => $(hostname -i) $AMBARI_SERVER_HOSTNAME
+
 #### Updating host details to ANSIBLE
 echo $AMBARI_SERVER_HOSTNAME 'ansible-ssh-user=ansible ansible-ssh-pass=ansible' >> /etc/ansible/hosts
 
 #### Create ansible local user and generate password-less ssh setup
 useradd ansible && echo ansible | passwd ansible --stdin
+
+### Add ANSIBLE user to SUDO
+visudo
+ansible ALL=(ALL)  NOPASSWD:ALL
+
+### Set hostname in current session
+hostname $AMBARI_SERVER_HOSTNAME
+
+### Restart the network service to set hostname permanently
+service network restart
+
+
+
+# As ANSIBLE user :
+# -----------------
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ""
+ssh-copy-id ansible@master.subex.com
 
 #### Create a home directory for the project
 mkdir hdp_roc && cd hdp_roc
