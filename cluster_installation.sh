ba1bf10bfeb7c7df8325c0037d7f6f8673262c6c
Feeroz Alam <feeroz123@gmail.com>
2018-11-28 17:10:04 +0530

    Updated changes for variables

--- cluster_installation.sh
+++ cluster_installation.sh
@@ -1,13 +1,13 @@
 echo "==============================================================================================================="
 echo "Deleting Blueprint"
-curl -s -H "X-Requested-By:ambari" -i -X DELETE -u admin:admin http://$AMBARI_SERVER_HOSTNAME:8080/api/v1/blueprints/$CLUSTER_NAME
+curl -s -H "X-Requested-By:ambari" -i -X DELETE -u admin:$AMBARI_ADMIN_PASSWORD http://$AMBARI_SERVER_HOSTNAME:8080/api/v1/blueprints/$CLUSTER_NAME
 echo "==============================================================================================================="
 echo "Register Blueprint"
-curl -s -H "X-Requested-By: ambari" -X POST -u admin:admin http://$AMBARI_SERVER_HOSTNAME:8080/api/v1/blueprints/$CLUSTER_NAME -d @$BLUEPRINT_FILE
+curl -s -H "X-Requested-By: ambari" -X POST -u admin:$AMBARI_ADMIN_PASSWORD http://$AMBARI_SERVER_HOSTNAME:8080/api/v1/blueprints/$CLUSTER_NAME -d @$BLUEPRINT_FILE
 echo "==============================================================================================================="
 echo "Create Cluster"
 
-curl -s -H "X-Requested-By: ambari" -X POST -u admin:admin http://$AMBARI_SERVER_HOSTNAME:8080/api/v1/clusters/$CLUSTER_NAME -d @hostmapping.json
+curl -s -H "X-Requested-By: ambari" -X POST -u admin:$AMBARI_ADMIN_PASSWORD http://$AMBARI_SERVER_HOSTNAME:8080/api/v1/clusters/$CLUSTER_NAME -d @hostmapping.json
 ret=$?
 if [ $ret == 0 ]
 then
@@ -18,14 +18,14 @@ else
 fi
 echo "==============================================================================================================="
 
-for id in `curl -s --user admin:admin -H 'X-Requested-By:ambari' -X GET http://$AMBARI_SERVER_HOSTNAME:8080/api/v1/clusters/$CLUSTER_NAME/requests/ | grep id | cut -d":" -f2`
+for id in `curl -s --user admin:$AMBARI_ADMIN_PASSWORD -H 'X-Requested-By:ambari' -X GET http://$AMBARI_SERVER_HOSTNAME:8080/api/v1/clusters/$CLUSTER_NAME/requests/ | grep id | cut -d":" -f2`
 do
 	echo "Tracking Request : $id " 
-	ProgressPercent=`curl -s --user admin:admin -H 'X-Requested-By:ambari' -X GET http://$AMBARI_SERVER_HOSTNAME:8080/api/v1/clusters/$CLUSTER_NAME/requests/$id | grep progress_percent | awk '{print $3}' | cut -d . -f 1`
+	ProgressPercent=`curl -s --user admin:$AMBARI_ADMIN_PASSWORD -H 'X-Requested-By:ambari' -X GET http://$AMBARI_SERVER_HOSTNAME:8080/api/v1/clusters/$CLUSTER_NAME/requests/$id | grep progress_percent | awk '{print $3}' | cut -d . -f 1`
 	echo " Progress: $ProgressPercent"
 	 
 	while [[ `echo $ProgressPercent | grep -v 100` ]]; do
-	  ProgressPercent=`curl -s --user admin:admin -H 'X-Requested-By:mycompany' -X GET http://$AMBARI_SERVER_HOSTNAME:8080/api/v1/clusters/$CLUSTER_NAME/requests/$id | grep progress_percent | awk '{print $3}' | cut -d . -f 1`
+	  ProgressPercent=`curl -s --user admin:$AMBARI_ADMIN_PASSWORD -H 'X-Requested-By:mycompany' -X GET http://$AMBARI_SERVER_HOSTNAME:8080/api/v1/clusters/$CLUSTER_NAME/requests/$id | grep progress_percent | awk '{print $3}' | cut -d . -f 1`
 	  tput cuu1
 	  echo " Progress: $ProgressPercent %"
 	  sleep 10
